5ada4d3f70a5ad28d977e26786aae58afde77da3
GabrielaRojasc <114246005+GabrielaRojasc@users.noreply.github.com>
2022-12-11 17:08:41 -0500

    Validación Score
    se agrega la lista score y el get

--- StartMovie-back-master/src/main/java/com/app/movie/repository/ScoreRepository.java
+++ StartMovie-back-master/src/main/java/com/app/movie/repository/ScoreRepository.java
@@ -10,6 +10,7 @@ import com.app.movie.interfaces.IScoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 
