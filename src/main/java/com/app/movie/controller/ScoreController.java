9402246788c64add3f4013544eef1a1d552c8b24
gabriela <gabriela.rojas.mt@usa.edu.co>
2022-12-11 16:37:40 -0500

    [validación score] se agrega la validación del NO CONTENT en el controller se agrega query por busqueda del nombre y email se agrega la lista score con el método getbymoviesandclient se crean los dos atributos de mensajes y 1 validación de califación de 1 a 5

---
+++ src/main/java/com/app/movie/controller/ScoreController.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.app.movie.controller;
+
+import com.app.movie.dto.ResponseDto;
+import com.app.movie.entities.Score;
+import com.app.movie.interfaces.IScoreRepository;
+import com.app.movie.service.ScoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/api/score")
+@CrossOrigin(origins = "*")
+public class ScoreController {
+
+    @Autowired
+    IScoreRepository repository;
+
+    public Iterable<Score> getAll(){
+        return repository.findAll();
+    }
+
+    public Optional<Score> findById(String id){
+        Optional<Score> response= repository.findById(id);
+        return response;
+    }
+
+    public List<Score> getByMoviesAndClient(String name, String email){
+        return repository.getScoreByMoviesAndClient(name, email);
+    }
+    public Boolean existsById(String id){
+        return repository.existsById(id);
+    }
+
+    public void deleteById(String id){
+        repository.deleteById(id);
+    }
+
+    public Score save(Score score){
+        return repository.save(score);
+    }
+
+}
