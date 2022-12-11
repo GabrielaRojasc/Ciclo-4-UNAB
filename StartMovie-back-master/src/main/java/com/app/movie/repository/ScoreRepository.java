e8a7d75ebe7e4affffaf5db4bb151a698c66db8f
GabrielaRojasc <114246005+GabrielaRojasc@users.noreply.github.com>
2022-12-11 16:53:36 -0500

    Repository
    se agrega la lista de score y la creación de del getByMoviesAndClient

--- StartMovie-back-master/src/main/java/com/app/movie/repository/ScoreRepository.java
+++ StartMovie-back-master/src/main/java/com/app/movie/repository/ScoreRepository.java
@@ -15,29 +15,32 @@ import java.util.Optional;
 
 @Repository
 public class ScoreRepository {
-    
+
     @Autowired
     IScoreRepository repository;
-    
+
     public Iterable<Score> getAll(){
         return repository.findAll();
     }
-    
+
     public Optional<Score> findById(String id){
         Optional<Score> response= repository.findById(id);
         return response;
     }
-    
+
+    public List<Score> getByMoviesAndClient(String name, String email){
+        return repository.getScoreByMoviesAndClient(name, email);
+    }
     public Boolean existsById(String id){
         return repository.existsById(id);
     }
-    
+
     public void deleteById(String id){
         repository.deleteById(id);
     }
-    
+
     public Score save(Score score){
         return repository.save(score);
     }
-    
+
 }
