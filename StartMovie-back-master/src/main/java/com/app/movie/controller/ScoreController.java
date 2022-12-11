65efe6c3cb58b3fd6c4bb5479ea896e6e01ec396
GabrielaRojasc <114246005+GabrielaRojasc@users.noreply.github.com>
2022-12-11 17:06:34 -0500

    Validacion score
    se agrega lista score y getByMoviesAndClient

--- StartMovie-back-master/src/main/java/com/app/movie/controller/ScoreController.java
+++ StartMovie-back-master/src/main/java/com/app/movie/controller/ScoreController.java
@@ -2,20 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.app.movie.controller;
+package com.app.movie.repository;
 
-import com.app.movie.dto.ResponseDto;
+
 import com.app.movie.entities.Score;
-import com.app.movie.service.ScoreService;
+import com.app.movie.interfaces.IScoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 
-@RestController
-@RequestMapping("/api/score")
-@CrossOrigin(origins = "*")
-public class ScoreController {
+@Repository
+public class ScoreRepository {
 
     @Autowired
     IScoreRepository repository;
