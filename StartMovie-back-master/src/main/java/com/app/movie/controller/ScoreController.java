48eb2a6a346d228c901be4c1bc2ac74520ab9e39
GabrielaRojasc <114246005+GabrielaRojasc@users.noreply.github.com>
2022-12-11 17:07:39 -0500

    Validación score
    Se agrega el NO CONTENT en el controller

--- StartMovie-back-master/src/main/java/com/app/movie/controller/ScoreController.java
+++ StartMovie-back-master/src/main/java/com/app/movie/controller/ScoreController.java
@@ -1,21 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.app.movie.repository;
+package com.app.movie.controller;
 
-
+import com.app.movie.dto.ResponseDto;
 import com.app.movie.entities.Score;
 import com.app.movie.interfaces.IScoreRepository;
+import com.app.movie.service.ScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 
-@Repository
-public class ScoreRepository {
+@RestController
+@RequestMapping("/api/score")
+@CrossOrigin(origins = "*")
+public class ScoreController {
 
     @Autowired
     IScoreRepository repository;
